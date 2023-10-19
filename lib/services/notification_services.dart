9b95518581db5e2302dccaa2737eec354b86e605
Divyank Sisodia <divyank1540.be20@chitkara.edu.in>
2023-10-19 09:14:46 +0530

    notification

---
+++ lib/services/notification_services.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class NotificationService {
+  static Future<void> initialize() async {
+    NotificationSettings settings =
+        await FirebaseMessaging.instance.requestPermission();
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      debugPrint('User granted permission');
+    }
+  }
+}
